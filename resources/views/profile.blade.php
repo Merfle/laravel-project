3b5891d1d23a8dfc24818d1d4f1ad7a889bc06f9
Merfle <joaquincuestas@hotmail.com>
2018-12-07 11:03:04 -0300

    bug fix profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,45 +3,27 @@
 @section('title', 'Profile')
 
 @section('content')
-<<<<<<< HEAD
-  <main class="main">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card">
-              <div class="card-header">Dashboard</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    You are logged in!
-                </div>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-              <form class="" action="index.html" method="post">
-                
-
-      </div>
-
-
-
-      </form>
-  </main>
-=======
   <div class="container">
+    <div class="card">
+      <div class="card-header">Dashboard</div>
+        <div class="card-body">
+          @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+              </div>
+            @endif
+        </div>
+    </div>
     <div class="card">
         <div class="card-header" id="dash" >Dashboard</div>
           <div class="card-body">
-              @if (session('status'))
-                  <div class="alert alert-success" role="alert">
-                      {{ session('status') }}
-                  </div>
-              @endif
-              <form>
-                <label for="project">Areas a rediseñar:</label><br>
+            @if (session('status'))
+              <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+              </div>
+            @endif
+            <form>
+              <label for="project">Areas a rediseñar:</label><br>
                 Piso
                 <input type="checkbox" name="piso" value="">
                 Pared
@@ -57,8 +39,8 @@
                 Metros cuadrados
                 <input type="text" name="metros" value=""><br>
                 <input type="submit" value="Submit">
-              </form>
-    </div>
-  </div>
->>>>>>> 92feadc5e396c17529a0cc603fcf106257e8766e
+            </form>
+          </div>
+        </div>
+      </div>
 @endsection
