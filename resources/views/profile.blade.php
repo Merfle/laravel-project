92feadc5e396c17529a0cc603fcf106257e8766e
stefanomartinez12 <stefomar12@gmail.com>
2018-12-07 10:35:49 -0300

    form de profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,15 +5,31 @@
 @section('content')
   <div class="container">
     <div class="card">
-        <div class="card-header">Dashboard</div>
+        <div class="card-header" id="dash" >Dashboard</div>
           <div class="card-body">
               @if (session('status'))
                   <div class="alert alert-success" role="alert">
                       {{ session('status') }}
                   </div>
               @endif
-              You are logged in!
-          </div>
+              <form>
+                <label for="project">Areas a rediseñar:</label><br>
+                Piso
+                <input type="checkbox" name="piso" value="">
+                Pared
+                <input type="checkbox" name="pared" value="">
+                Techo
+                <input type="checkbox" name="techo" value="">
+                Placard
+                <input type="checkbox" name="placard" value="">
+                Ventana
+                <input type="checkbox" name="ventana" value="">
+                Cortina
+                <input type="checkbox" name="cortina" value=""><br>
+                Metros cuadrados
+                <input type="text" name="metros" value=""><br>
+                <input type="submit" value="Submit">
+              </form>
     </div>
   </div>
 @endsection
