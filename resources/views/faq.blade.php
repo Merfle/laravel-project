4668977f40a71c062fedaf9bc99fc5ac23cb0e85
Patricio Ornague <Patricio@Patricios-MacBook-Pro-2.local>
2018-12-19 08:48:08 -0300

    arreglos picantes

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -17,8 +17,8 @@
       </div>
       <div class="card-2">
         <div class="card-data-2">
-          <h2>Lorem ipsum dolor?</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h2>¿Cómo puedo realizar un proyecto con ustedes?</h2>
+          <p>Para poder realizar un proyecto con <strong>Tona</strong>, podes registrarte en nuestra página para así poder crear proyectos personalizados quue se adapten a tus preferencias. Otras opciones incluyen contacto por mail a <a href="mailto:tonadesign@gmail.com" target="_top">tonadesign@gmail.com</a>, o  podes realizar una entrevista en persona en nuestras oficinas.</p>
         </div>
       </div>
       <div class="card-3">
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -7,7 +7,7 @@
         <div class="card-1">
           <div class="card-data-1">
             <h2>eventos</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <p>Corporativos o privados</p>
           </div>
           <div class="photos-1">
             <img src="../img/F1-25.jpg" alt="foto-boceto" style="width:100%;">
@@ -16,7 +16,7 @@
         <div class="card-2">
           <div class="card-data-2">
             <h2>hogar</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+            <p>Diseño/Rediseño para el hogar</p>
           </div>
           <div class="photos-2">
             <img src="../img/event-1.jpg" alt="foto-boceto" style="width:100%">
@@ -25,7 +25,7 @@
         <div class="card-3">
           <div class="card-data-3">
             <h2>espacios de trabajo</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+            <p>Design Thinking para mejorar tanto la calidad como la cultura del trabajo</p>
           </div>
           <div class="photos-3">
             <img src="../img/library-1.jpeg" alt="foto-boceto" width="100%">
