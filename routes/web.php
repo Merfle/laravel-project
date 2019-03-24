4e69276a3543ebe2615f2ccdbd16cc68a371fa12
Patricio Ornague <Patricio@Patricios-MacBook-Pro-2.local>
2019-03-24 14:54:05 -0300

    dominio de laravel

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,15 @@
 
 Auth::routes();
 
-Route::get('miliadeco.com/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('miliadeco.com/faq', 'FaqController@index');
+Route::get('/faq', 'FaqController@index');
 
-Route::get('miliadeco.com/nosotros', 'NosotrosController@index');
+Route::get('/nosotros', 'NosotrosController@index');
 
-Route::get('miliadeco.com/portfolio', 'PortfolioController@index');
+Route::get('/portfolio', 'PortfolioController@index');
 
-Route::get('miliadeco.com/contacto', 'ContactoController@index');
+Route::get('/contacto', 'ContactoController@index');
 
-Route::get('miliadeco.com/profile', 'ProfileController@index')->name('profile');
-Route::post('miliadeco.com/profile', 'RenovacionesController@store');
+Route::get('/profile', 'ProfileController@index')->name('profile');
+Route::post('/profile', 'RenovacionesController@store');
